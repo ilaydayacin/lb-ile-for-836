d7bbbcd295446127fbbd84a524b3070e868eea7b
ilayda yalçın <146716704+ilaydayacin@users.noreply.github.com>
2023-10-24 16:00:52 +0300

    Add files via upload

---
+++ random 2-836/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace random_2_836
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbNotlar = new System.Windows.Forms.ListBox();
+            this.btnNotVer = new System.Windows.Forms.Button();
+            this.lbGecti = new System.Windows.Forms.ListBox();
+            this.lbTesekkur = new System.Windows.Forms.ListBox();
+            this.lbKaldi = new System.Windows.Forms.ListBox();
+            this.lblKaldi = new System.Windows.Forms.Label();
+            this.lblGecti = new System.Windows.Forms.Label();
+            this.lblTesekkur = new System.Windows.Forms.Label();
+            this.lblTakdir = new System.Windows.Forms.Label();
+            this.lbTakdir = new System.Windows.Forms.ListBox();
+            this.lblSayi = new System.Windows.Forms.Label();
+            this.btnDurumcuk = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbNotlar
+            // 
+            this.lbNotlar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbNotlar.FormattingEnabled = true;
+            this.lbNotlar.ItemHeight = 25;
+            this.lbNotlar.Location = new System.Drawing.Point(218, 12);
+            this.lbNotlar.Name = "lbNotlar";
+            this.lbNotlar.Size = new System.Drawing.Size(96, 404);
+            this.lbNotlar.TabIndex = 3;
+            // 
+            // btnNotVer
+            // 
+            this.btnNotVer.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnNotVer.Location = new System.Drawing.Point(15, 12);
+            this.btnNotVer.Name = "btnNotVer";
+            this.btnNotVer.Size = new System.Drawing.Size(197, 82);
+            this.btnNotVer.TabIndex = 2;
+            this.btnNotVer.Text = "Not ver";
+            this.btnNotVer.UseVisualStyleBackColor = true;
+            this.btnNotVer.Click += new System.EventHandler(this.btnNotVer_Click);
+            // 
+            // lbGecti
+            // 
+            this.lbGecti.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbGecti.FormattingEnabled = true;
+            this.lbGecti.ItemHeight = 25;
+            this.lbGecti.Location = new System.Drawing.Point(427, 12);
+            this.lbGecti.Name = "lbGecti";
+            this.lbGecti.Size = new System.Drawing.Size(89, 404);
+            this.lbGecti.TabIndex = 4;
+            // 
+            // lbTesekkur
+            // 
+            this.lbTesekkur.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbTesekkur.FormattingEnabled = true;
+            this.lbTesekkur.ItemHeight = 25;
+            this.lbTesekkur.Location = new System.Drawing.Point(522, 12);
+            this.lbTesekkur.Name = "lbTesekkur";
+            this.lbTesekkur.Size = new System.Drawing.Size(91, 404);
+            this.lbTesekkur.TabIndex = 5;
+            // 
+            // lbKaldi
+            // 
+            this.lbKaldi.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbKaldi.FormattingEnabled = true;
+            this.lbKaldi.ItemHeight = 25;
+            this.lbKaldi.Location = new System.Drawing.Point(320, 12);
+            this.lbKaldi.Name = "lbKaldi";
+            this.lbKaldi.Size = new System.Drawing.Size(101, 404);
+            this.lbKaldi.TabIndex = 7;
+            // 
+            // lblKaldi
+            // 
+            this.lblKaldi.AutoSize = true;
+            this.lblKaldi.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblKaldi.Location = new System.Drawing.Point(316, 431);
+            this.lblKaldi.Name = "lblKaldi";
+            this.lblKaldi.Size = new System.Drawing.Size(56, 24);
+            this.lblKaldi.TabIndex = 9;
+            this.lblKaldi.Text = "Kaldı:";
+            // 
+            // lblGecti
+            // 
+            this.lblGecti.AutoSize = true;
+            this.lblGecti.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblGecti.Location = new System.Drawing.Point(423, 431);
+            this.lblGecti.Name = "lblGecti";
+            this.lblGecti.Size = new System.Drawing.Size(63, 24);
+            this.lblGecti.TabIndex = 10;
+            this.lblGecti.Text = "Geçti: ";
+            // 
+            // lblTesekkur
+            // 
+            this.lblTesekkur.AutoSize = true;
+            this.lblTesekkur.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblTesekkur.Location = new System.Drawing.Point(518, 431);
+            this.lblTesekkur.Name = "lblTesekkur";
+            this.lblTesekkur.Size = new System.Drawing.Size(93, 24);
+            this.lblTesekkur.TabIndex = 11;
+            this.lblTesekkur.Text = "Teşekkür:";
+            // 
+            // lblTakdir
+            // 
+            this.lblTakdir.AutoSize = true;
+            this.lblTakdir.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblTakdir.Location = new System.Drawing.Point(651, 431);
+            this.lblTakdir.Name = "lblTakdir";
+            this.lblTakdir.Size = new System.Drawing.Size(67, 24);
+            this.lblTakdir.TabIndex = 12;
+            this.lblTakdir.Text = "Takdir:";
+            // 
+            // lbTakdir
+            // 
+            this.lbTakdir.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbTakdir.FormattingEnabled = true;
+            this.lbTakdir.ItemHeight = 25;
+            this.lbTakdir.Location = new System.Drawing.Point(627, 12);
+            this.lbTakdir.Name = "lbTakdir";
+            this.lbTakdir.Size = new System.Drawing.Size(91, 404);
+            this.lbTakdir.TabIndex = 13;
+            // 
+            // lblSayi
+            // 
+            this.lblSayi.AutoSize = true;
+            this.lblSayi.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSayi.Location = new System.Drawing.Point(112, 431);
+            this.lblSayi.Name = "lblSayi";
+            this.lblSayi.Size = new System.Drawing.Size(141, 24);
+            this.lblSayi.TabIndex = 14;
+            this.lblSayi.Text = "Öğrenci Sayısı: ";
+            // 
+            // btnDurumcuk
+            // 
+            this.btnDurumcuk.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnDurumcuk.Location = new System.Drawing.Point(15, 119);
+            this.btnDurumcuk.Name = "btnDurumcuk";
+            this.btnDurumcuk.Size = new System.Drawing.Size(197, 89);
+            this.btnDurumcuk.TabIndex = 15;
+            this.btnDurumcuk.Text = "Durum";
+            this.btnDurumcuk.UseVisualStyleBackColor = true;
+            this.btnDurumcuk.Click += new System.EventHandler(this.btnDurumcuk_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(990, 572);
+            this.Controls.Add(this.btnDurumcuk);
+            this.Controls.Add(this.lblSayi);
+            this.Controls.Add(this.lbTakdir);
+            this.Controls.Add(this.lblTakdir);
+            this.Controls.Add(this.lblTesekkur);
+            this.Controls.Add(this.lblGecti);
+            this.Controls.Add(this.lblKaldi);
+            this.Controls.Add(this.lbKaldi);
+            this.Controls.Add(this.lbTesekkur);
+            this.Controls.Add(this.lbGecti);
+            this.Controls.Add(this.lbNotlar);
+            this.Controls.Add(this.btnNotVer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbNotlar;
+        private System.Windows.Forms.Button btnNotVer;
+        private System.Windows.Forms.ListBox lbGecti;
+        private System.Windows.Forms.ListBox lbTesekkur;
+        private System.Windows.Forms.ListBox lbKaldi;
+        private System.Windows.Forms.Button btnDurum;
+        private System.Windows.Forms.Label lblKaldi;
+        private System.Windows.Forms.Label lblGecti;
+        private System.Windows.Forms.Label lblTesekkur;
+        private System.Windows.Forms.Label lblTakdir;
+        private System.Windows.Forms.ListBox lbTakdir;
+        private System.Windows.Forms.Label lblSayi;
+        private System.Windows.Forms.Button btnDurumcuk;
+    }
+}
+
---
+++ random 2-836/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace random_2_836
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNotVer_Click(object sender, EventArgs e)
+        {
+            lbTakdir.Items.Clear();
+            lbTesekkur.Items.Clear();
+            lbKaldi.Items.Clear();
+            lbGecti.Items.Clear();
+            lbNotlar.Items.Clear();
+            Random rnd =new Random();
+
+            lblGecti.Text = "";
+            lblKaldi.Text = "";
+            lblTakdir.Text = "";
+            lblTesekkur.Text = "";
+            btnDurumcuk.Enabled = true;
+
+            for (int i = 0; i < 15; i++)
+            {
+                int Not = rnd.Next(0,101);
+                lbNotlar.Items.Add(Not);
+            }
+
+        }
+
+        private void btnDurumcuk_Click(object sender, EventArgs e)
+        {
+            int kaldi = 0, gecti = 0, takdir = 0, tesekkur = 0;
+
+
+            for (int i = 0; i < lbNotlar.Items.Count; i++)
+            {
+                int not = Convert.ToInt32(lbNotlar.Items[i].ToString());
+
+                if (not < 50)
+                {
+                    lbKaldi.Items.Add(not);
+                    lblKaldi.Text = "Kaldı: " + lbKaldi.Items.Count.ToString();
+                    kaldi++;
+
+                }
+                else if (not >= 50 && not < 70)
+                {
+                    lbGecti.Items.Add(not);
+                    gecti++;
+                    lblGecti.Text = "Geçti: " + lbGecti.Items.Count.ToString();
+                }
+                else if (not >= 70 && not < 85)
+                {
+                    lbTesekkur.Items.Add(not);
+                    tesekkur++;
+                    lblTesekkur.Text = "Teşekkür: " + lbTesekkur.Items.Count.ToString();
+                }
+                else
+                {
+                    lbTakdir.Items.Add(not);
+                    takdir++;
+                    lblTakdir.Text = "Takdir: " + lbTakdir.Items.Count.ToString();
+                }
+                lblSayi.Text = "Öğrenci Sayısı: " + lbNotlar.Items.Count.ToString();
+            }
+            btnDurumcuk.Enabled = false;
+        }
+    }
+}
